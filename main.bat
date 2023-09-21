5280c3a1f22ea7010249478018084a5f05d2422c
Jocelyn Castellano <admin@malwarefight.wip.la>
2023-09-21 11:34:03 -0700

    Update main.bat

--- main.bat
+++ main.bat
@@ -5,11 +5,11 @@ set PATH=%PATH%;%cd%
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 cd ..
 mkdir build && cd build
-gclient help
+call gclient help
 mkdir chromium && cd chromium
 git config --global user.name "My Name"
 git config --global user.email "noreply@not-a-real-email.com"
 git config --global core.autocrlf false
 git config --global core.filemode false
 git config --global branch.autosetuprebase always
-fetch --help
+call fetch --help
