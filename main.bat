33b5d551fc9bea70b70693e45d8042ee7f66046d
Jocelyn Castellano <admin@malwarefight.wip.la>
2023-09-21 11:19:28 -0700

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,5 +1,8 @@
 mkdir depot_tools && cd depot_tools
 curl https://storage.googleapis.com/chrome-infra/depot_tools.zip > depot_tools.zip
 tar -xf depot_tools.zip
+set PATH=%PATH%;%cd%
+set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 cd ..
-tree
+mkdir build
+gclient
