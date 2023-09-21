69216d9621c85c9345ee2ccbcf2fa008a8c4686b
Jocelyn Castellano <admin@malwarefight.wip.la>
2023-09-21 16:45:46 -0700

    Update main.bat

--- main.bat
+++ main.bat
@@ -6,6 +6,6 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 cd ..
 mkdir build && cd build
 mkdir chromium && cd chromium
-curl https://gsdview.appspot.com/chromium-browser-official/chromium-117.0.5938.88.tar.xz > chromium.tar.xz
+curl -L https://gsdview.appspot.com/chromium-browser-official/chromium-117.0.5938.88.tar.xz > chromium.tar.xz
 tar -xf chromium.tar.xz
 tree
