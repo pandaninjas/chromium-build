39ca3473ff25923e57f1a89c1684db21f2544329
Jocelyn Castellano <admin@malwarefight.wip.la>
2023-09-21 11:25:16 -0700

    Update main.bat

--- main.bat
+++ main.bat
@@ -6,3 +6,10 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 cd ..
 mkdir build && cd build
 gclient
+mkdir chromium && cd chromium
+git config --global user.name "My Name"
+git config --global user.email "noreply@not-a-real-email.com"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global branch.autosetuprebase always
+fetch --help
