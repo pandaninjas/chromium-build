90f323b367dd96383dbcaca0032edbfc874d09de
Jocelyn Castellano <admin@malwarefight.wip.la>
2023-09-22 11:19:03 -0700

    Fix things

--- main.bat
+++ main.bat
@@ -5,7 +5,11 @@ set PATH=%PATH%;%cd%
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 cd ..
 mkdir build && cd build
+call gclient help
 mkdir chromium && cd chromium
-curl -L https://gsdview.appspot.com/chromium-browser-official/chromium-117.0.5938.88.tar.xz > chromium.tar.xz
-tar -xf chromium.tar.xz
-tree
+git config --global user.name "My Name"
+git config --global user.email "noreply@not-a-real-email.com"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global branch.autosetuprebase always
+call fetch --no-history chromium
